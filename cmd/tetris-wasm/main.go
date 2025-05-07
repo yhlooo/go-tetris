248c9e78fa815b65b98308a19cccec76986667ec
yhlooo <yhlooo.x@outlook.com>
2025-05-07 23:57:51 +0800

    chore: 添加 github pages 配置

--- cmd/tetris-wasm/main.go
+++ cmd/tetris-wasm/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -9,20 +10,39 @@ import (
 	"github.com/yhlooo/go-tetris/pkg/ui/web"
 )
 
+var (
+	static bool
+)
+
+func init() {
+	flag.BoolVar(&static, "static", false, "generate static files")
+}
+
 // The main function is the entry point where the app is configured and started.
 // It is executed in 2 different environments: A client (the web browser) and a
 // server.
 func main() {
+	flag.Parse()
+
 	app.Route("/", func() app.Composer {
 		return web.NewGameUI()
 	})
 	app.RunWhenOnBrowser()
 
-	http.Handle("/", &app.Handler{
-		Name: "Tetris",
-	})
-
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		log.Fatal(err)
+	if static {
+		log.Println("generate static files to ./web")
+		if err := app.GenerateStaticWebsite("web", &app.Handler{
+			Name: "Tetris",
+		}); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Println("serving http on :8000")
+		http.Handle("/", &app.Handler{
+			Name: "Tetris",
+		})
+		if err := http.ListenAndServe(":8000", nil); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
